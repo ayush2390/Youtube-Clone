c625a8d88f9b89af788db40c4a60bc24d1d0f733
Ayush Thakur <tsayush2390@gmail.com>
2024-02-03 17:44:36 +0530

    Update VideoCard.js

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -24,7 +24,7 @@ const VideoCard = ({video :{id: {videoId}, snippet}}) => {
         </Link>
       <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
             <Typography variant='subtitle2' fontWeight='bold' color='gray'>
-                {snippet?.channeltitle || demoChannelTitle}
+                {snippet?.channelTitle || demoChannelTitle}
                 <CheckCircle sx={{fontSize: 12, color: 'gray', ml: '5px'}}/>
             </Typography> 
         </Link>
