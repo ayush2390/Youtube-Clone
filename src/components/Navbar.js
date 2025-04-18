32bcd9f10d1a119ee106c83bd645b3741307f45d
tsayush2390 <115918401+tsayush2390@users.noreply.github.com>
2025-04-18 14:28:35 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,10 +11,7 @@ const Navbar = () => (
     p={2}
     sx={{position:'sticky', background: '#000', top: 0, justifyContent: 'space-between'}}
      >
-        <Link to='/' style={{display: 'flex', alignItems:'center'}}>
-            <img src={logo} alt='logo' height={45}/>
-        </Link>
-        <SearchBar />
+       
     </Stack>
   )
 
